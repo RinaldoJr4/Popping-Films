6fcd24626b5759689a1c56204c13ffd7e30e4bdd
Rinaldo Jr <102991159+RinaldoJr3@users.noreply.github.com>
2022-07-14 16:13:59 -0300

    Adicionando ultimos detalhes para a detailsViewController

--- Popping Films/Controllers/DetailsViewController.swift
+++ Popping Films/Controllers/DetailsViewController.swift
@@ -9,13 +9,31 @@ import UIKit
 
 class DetailsViewController: UIViewController {
 
+    @IBOutlet var backdropImage: UIImageView!
+    @IBOutlet var titleLabel: UILabel!
+    
+    @IBOutlet var posterImage: UIImageView!
+    @IBOutlet var ratingLabel: UILabel!
+    
+    @IBOutlet var overviewLabel: UILabel!
+    
     var movie: Movie?
     
     override func viewDidLoad() {
+        // Do any additional setup after loading the view.
         super.viewDidLoad()
         
-        self.title = movie?.title
-        // Do any additional setup after loading the view.
+        guard let movie = movie else {
+            return
+        }
+        self.title = movie.title
+        backdropImage.image = UIImage(named: movie.backdrop)
+        posterImage.image = UIImage(named: movie.poster)
+        titleLabel.text = movie.title
+        ratingLabel.text = "Rating: \(movie.voteAverage)/10"
+        overviewLabel.text = movie.overview
+        
+
     }
     
 
